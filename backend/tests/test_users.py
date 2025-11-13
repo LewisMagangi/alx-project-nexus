0f9183dcdffb87aed7aa55fdce6cd18e241f39d3
Liquelaliqour <lewismomanyi34@gmail.com>
2025-11-13 18:24:43 +0300

    test(api): Added pytest and pytest django for testing apis

---
+++ backend/tests/test_users.py
@@ -0,0 +1,8 @@
+import pytest
+from users.models import User
+
+@pytest.mark.django_db
+def test_create_user():
+    user = User.objects.create(username='newuser', password='newpass')
+    assert user.username == 'newuser'
+    assert User.objects.count() == 1
