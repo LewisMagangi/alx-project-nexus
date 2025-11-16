8e17c5286cf3c745fdfc5b919776a098b035aa2a
Liquelaliqour <lewismomanyi34@gmail.com>
2025-11-16 22:07:51 +0300

    style(isort): Fixing Linting Errors

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
