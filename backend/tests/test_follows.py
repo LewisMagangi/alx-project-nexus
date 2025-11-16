0d5c20d4db50b62ed208ec2bace763fc00add3e5
Liquelaliqour <lewismomanyi34@gmail.com>
2025-11-16 22:40:38 +0300

    fix: authenticate client in test_follow_user to pass authentication requirement

--- backend/tests/test_follows.py
+++ backend/tests/test_follows.py
@@ -9,6 +9,7 @@ def test_follow_user():
     follower = User.objects.create(username="follower", password="pass")
     following = User.objects.create(username="following", password="pass")
     client = APIClient()
+    client.force_authenticate(user=follower)
     url = reverse("follow-list")
     data = {"follower": follower.id, "following": following.id}
     response = client.post(url, data, format="json")
