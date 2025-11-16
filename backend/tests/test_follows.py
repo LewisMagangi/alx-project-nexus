94dee82c532deda6e749bd114c7edc8c55388925
Liquelaliqour <lewismomanyi34@gmail.com>
2025-11-16 22:17:19 +0300

    style(isort): Fixed isort issues

--- backend/tests/test_follows.py
+++ backend/tests/test_follows.py
@@ -1,7 +1,7 @@
 import pytest
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APIClient
-from users.models import User
 
 
 @pytest.mark.django_db
