37f711840def89f2e8f98e30a068e037c6aee083
Liquelaliqour <lewismomanyi34@gmail.com>
2025-11-26 21:21:49 +0300

    style(fix): removed unused imports to adhere to flake8 standards

--- backend/tests/test_usermessages.py
+++ backend/tests/test_usermessages.py
@@ -1,6 +1,5 @@
 import pytest
 from django.contrib.auth.models import User
-from django.urls import reverse
 from rest_framework.test import APIClient
 from usermessages.models import Message
 
--- backend/usermessages/views.py
+++ backend/usermessages/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Message
 from .serializers import MessageSerializer
 
 
