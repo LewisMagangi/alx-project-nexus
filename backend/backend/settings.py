adf9d88b6a268f1c21ade5a023fd969583a0e55c
Liquelaliqour <lewismomanyi34@gmail.com>
2026-01-21 21:27:08 +0300

    fix: remove communities from INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,7 +113,6 @@ INSTALLED_APPS = [
     "notifications",
     "search",
     "usermessages",
-    "communities",
     "legal",
     "hashtags",
     "blocks",
@@ -378,9 +377,11 @@ EMAIL_BACKEND = os.getenv(
 # Email configuration
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = (
-    os.getenv("EMAIL_USE_TLS", "True").lower() in ["true", "1", "yes"]
-)
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() in [
+    "true",
+    "1",
+    "yes",
+]
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@nexus.com")
